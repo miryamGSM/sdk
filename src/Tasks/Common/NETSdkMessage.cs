2d9ed666d7de54753bc3a09f94dcedea2c41e4e3
Livar Cunha <livar@live.com>
2018-02-16 09:53:20 -0800

    Displaying a message whenever a build happens using a preview version of the CLI. Also replaced NETSdkError with NETSdkMessage. This message will be driven by a property set by the CLI.

--- src/Tasks/Common/NETSdkMessage.cs
+++ src/Tasks/Common/NETSdkMessage.cs
@@ -1,24 +1,25 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Build.Framework;
+using System;
 using System.Globalization;
 
 namespace Microsoft.NET.Build.Tasks
 {
     /// <summary>
-    /// Provides a localizable mechanism for logging an error from the SDK targets.
+    /// Provides a localizable mechanism for logging an messages of different kids from the SDK targets.
     /// </summary>
     public class
 #if EXTENSIONS
         // This task source is shared with multiple task Dlls.  Since both tasks
         // may be loaded into the same project and each task accesses only resources
         // in its own assembly they must have a unique name so-as not to clash.
-        NETBuildExtensionsError
+        NETBuildExtensionsMessage
 #else
-        NETSdkError 
+        NETSdkMessage
 #endif
-        : TaskBase
+     : TaskBase
     {
         /// <summary>
         /// The name of the resource in Strings.resx that contains the desired error message.
@@ -31,7 +32,9 @@ namespace Microsoft.NET.Build.Tasks
         /// </summary>
         public string[] FormatArguments { get; set; }
 
-        public bool WarningOnly { get; set; }
+        public string Severity { get; set; } = "Info";
+
+        public string Importance { get; set; } = "Normal";
 
         private static readonly string[] EmptyArguments = new[] { "" };
 
@@ -46,16 +49,24 @@ namespace Microsoft.NET.Build.Tasks
                 FormatArguments = EmptyArguments;
             }
 
+            DiagnosticMessageSeverity severity =
+                (DiagnosticMessageSeverity)Enum.Parse(typeof(DiagnosticMessageSeverity), Severity, true);
+            MessageImportance importance = (MessageImportance)Enum.Parse(typeof(MessageImportance), Importance, true);
+
             string format = Strings.ResourceManager.GetString(ResourceName, Strings.Culture);
             string message = string.Format(CultureInfo.CurrentCulture, format, FormatArguments);
 
-            if (WarningOnly)
+            switch (severity)
             {
-                Log.LogWarning(message);
-            }
-            else
-            {
-                Log.LogError(message);
+                case DiagnosticMessageSeverity.Error:
+                    Log.LogError(message);
+                    break;
+                case DiagnosticMessageSeverity.Warning:
+                    Log.LogWarning(message);
+                    break;
+                case DiagnosticMessageSeverity.Info:
+                    Log.LogMessage(importance, message);
+                    break;
             }
         }
     }
